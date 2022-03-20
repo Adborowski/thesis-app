b114298d7f884f95aba6a40dfd3058f1655bcc34
Adam Borowski <adabo@itu.dk>
2022-03-20 16:01:03 +0100

    Added empty states and handlers to TaskEditor

---
+++ src/components/Marker/TaskPopup.js
@@ -0,0 +1,8 @@
+import { Popup } from "react-leaflet";
+
+const TaskPopup = (props) => {
+  const task = props.task;
+  return <p>I am popup</p>;
+};
+
+export default TaskPopup;
--- src/components/TaskEditor/TaskEditor.js
+++ src/components/TaskEditor/TaskEditor.js
@@ -1,17 +1,49 @@
 import classes from "./TaskEditor.module.css";
-
+import { useState } from "react";
 // this is the content of the task editor popup
-// the TaskEditor is
+
 const TaskEditor = (props) => {
+  const [taskTitle, setTaskTitle] = useState();
+  const [taskDescription, setTaskDescription] = useState();
+  const [taskReward, setTaskReward] = useState();
+  const [taskLatlng, setTaskLatlng] = useState(props.latlng);
+  const handleTitleChange = (e) => {};
+
+  const handleRewardChange = (e) => {};
+
+  const handleDescriptionChange = (e) => {};
+
+  const handleTaskSubmit = (e) => {};
+
   if (props.latlng) {
     return (
       <div className={classes.TaskEditor}>
         <h3>Task name</h3>
         <form>
-          <input id="taskTitle" placeholder={"task title"}></input>
-          <input id="taskReward" type="number" placeholder={"reward"}></input>
-          <input id="taskDescription" placeholder={"task description"}></input>
-          <div id="btnCreateTask" className={"button"}>
+          <input
+            onChange={handleTitleChange}
+            id="taskTitle"
+            placeholder={"task title"}
+          ></input>
+
+          <input
+            onChange={handleRewardChange}
+            id="taskReward"
+            type="number"
+            placeholder={"reward"}
+          ></input>
+
+          <input
+            onChange={handleDescriptionChange}
+            id="taskDescription"
+            placeholder={"task description"}
+          ></input>
+
+          <div
+            onClick={handleTaskSubmit}
+            id="btnCreateTask"
+            className={"button"}
+          >
             Create
           </div>
         </form>
