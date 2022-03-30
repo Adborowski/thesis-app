3bb5d0523a7444132e626df734f29b9ef9e52251
Adam Borowski <adabo@itu.dk>
2022-03-30 16:17:16 +0200

    Added task media to SolveEditor

--- src/components/SolveEditor/SolveEditor.js
+++ src/components/SolveEditor/SolveEditor.js
@@ -8,22 +8,58 @@ import Icon from "../Icons/Icon";
 import Minimap from "../Minimap/Minimap";
 
 const SolveEditor = (props) => {
-  const [taskToSolve, setTaskToSolve] = useState(props.task);
+  const [task, setTask] = useState(props.task);
   console.log(props);
 
   useEffect(() => {
     if (props.task) {
-      setTaskToSolve(props.task);
+      setTask(props.task);
       console.log(props);
     }
   }, [props]);
 
-  if (taskToSolve) {
+  if (task) {
     return (
       <div className={classes.SolveEditor}>
         <Nav />
         <h1 className={classes.header}>Solve Task</h1>
-        <Minimap taskLatlng={taskToSolve.latlng} icon={Icon} />
+
+        <div className={classes.latlng}>
+          <div className={classes.position}>
+            {task ? task.latlng[0] : "dummy"}
+          </div>
+          <div className={classes.position}>
+            {task ? task.latlng[1] : "dummy"}
+          </div>
+        </div>
+
+        <Minimap taskLatlng={task.latlng} icon={Icon} />
+
+        <div className={classes.taskInfo}>
+          <div className={classes.infoItem}>
+            <div className={classes.label}>Title</div>
+            <div className={classes.content}>{task.title}</div>
+          </div>
+          <div className={classes.infoItem}>
+            <div className={classes.label}>Description</div>
+            <div className={classes.content}>{task.description}</div>
+          </div>
+          <div className={classes.infoItem}>
+            <div className={classes.label}>Reward</div>
+            <div className={classes.content}>{task.reward}</div>
+          </div>
+        </div>
+
+        <h1 className={classes.header}>Task Media</h1>
+        <div className={classes.taskMedia}>
+          {task.media.map((item) => (
+            <div
+              key={item}
+              style={{ backgroundImage: `url(img/${item})` }}
+              className={classes.mediaItem}
+            ></div>
+          ))}
+        </div>
 
         <div className={classes.controls}></div>
       </div>
